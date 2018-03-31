c07f564a411d1e2967f076d59055a24285918f7f
Vinícius Dantas <viniciusdantas@bct.ect.ufrn.br>
2018-03-31 10:54:21 -0400

    Add initial test suite

---
+++ cards/deck_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 16 {
+		t.Errorf("Expected deck length of 16, but got %d", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %s", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected last card of Four of Clubs, but got %s", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck, _ := newDeckFromFile("_decktesting")
+
+	for i, _ := range deck {
+		if deck[i] != loadedDeck[i] {
+			t.Errorf("Expected the %d-th card to be %s", i, deck[i])
+		}
+	}
+
+	os.Remove("_decktesting")
+}
