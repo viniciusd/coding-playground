91828518f9f38539f4a27090478aad96b36c159b
Vinícius Dantas <viniciusdantas@bct.ect.ufrn.br>
2018-04-01 14:22:20 -0400

    Add a map[string]func

--- map/main.go
+++ map/main.go
@@ -3,10 +3,22 @@ package main
 import "fmt"
 
 func main() {
+	funcs := map[string]func(c map[string]string) int{
+		"print": printMap,
+	}
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#00ff00",
+		"white": "#ffffff",
+		"black": "#000000",
 	}
 
-	fmt.Println(colors)
+	funcs["print"](colors)
+}
+
+func printMap(c map[string]string) int {
+	for color, hex := range c {
+		fmt.Println(color, hex)
+	}
+	return 42
 }
