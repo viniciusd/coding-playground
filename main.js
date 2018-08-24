8a3e1a4c04d90e945488a6665d839f6096dc9586
Vinícius Dantas <viniciusdantas@bct.ect.ufrn.br>
2018-08-23 23:21:49 -0400

    wasm: Make wasmFactory return a promise

--- main.js
+++ main.js
@@ -1,33 +1,48 @@
-async function moduleFactory(id) {
+(async (global) => {
+    function wasmFactory(id) {
+        let tempContext = {
+            Module: {
+                print: function() {
+                    let text = Array.prototype.slice.call(arguments).join(' ');
+                    console.log(id, text);
+                    document.getElementById("result").innerHTML += "<br>\n" + text;
+                },
+                printErr: console.error
+            }
+        };
+        return loadWasm.call(tempContext);
+    }
 
-    var wasmWrapper = await (await fetch('hello.js')).text();
+    global.wasmFactory = wasmFactory;
 
-    let tempContext = {
-        Module: {
-            print: function() {
-                let text = Array.prototype.slice.call(arguments).join(' ');
-                console.log(id, text);
-                document.getElementById("result").innerHTML += "<br>\n" + text;
-            },
-            printErr: console.error
-        }
-    };
+    let wasmWrapper = await (await fetch('hello.js')).text();
+    let loadWasm = new Function(`
+        var Module = this.Module;
+        let _resolve;
+        let promise = new Promise((resolve, reject) => _resolve = resolve);
+        Module.postRun = (Module.postRun || []).concat([
+            _resolve.bind(promise, Module)
+        ]); 
+        ${wasmWrapper};
+        return promise; 
+    `);
 
-    (function(str) {
-      return eval('var Module = this.Module;'+str);
-    }.call(tempContext, wasmWrapper));
-
-    return tempContext.Module;
-}
+})(window);
 
 var hello1, hello2;
-moduleFactory(1).then((m) => hello1 = m);
-moduleFactory(2).then((m) => hello2 = m);
 
 function click1() {
-    hello1._hello()
+    if (typeof hello1 === 'undefined') {
+        wasmFactory(1).then(m => hello1 = m);
+    } else {
+        hello1._hello()
+    }
 }
 
 function click2() {
-    hello2._hello()
+    if (typeof hello2 === 'undefined') {
+        wasmFactory(1).then(m => hello2 = m);
+    } else {
+        hello2._hello()
+    }
 }
